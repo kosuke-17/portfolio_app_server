26eacbb38b0f437cbebb375c323673d442f9cbcd
kosuke-17 <tamutamu17@icloud.com>
2022-09-22 11:43:20 +0900

    swagger作成

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,31 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { writeFile } from 'fs/promises';
 import { AppModule } from './app.module';
+// import { isDev } from './lib/checkEnv';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  const app: INestApplication = await NestFactory.create(AppModule);
+
+  // http://localhost:3031/apiのようにdefault
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  // if (isDev) {
+  const config = new DocumentBuilder()
+    .setTitle('portfolio_app ドキュメント')
+    .build();
+
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
+  // }
+  app.enableCors();
+
+  // if (isDev) {
+  await writeFile('./swagger.json', JSON.stringify(document, null, '  '));
+  // }
+
+  await app.listen(3031);
 }
 bootstrap();
