b247f2c129adb76c79741c8a578d8a48580000da
kosuke-17 <tamutamu17@icloud.com>
2022-09-22 12:24:26 +0900

    リファクタリング

---
+++ server/src/createApp.ts
@@ -0,0 +1,27 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+// applicationの作成
+export const createApp = async () => {
+  const app: INestApplication = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const document: OpenAPIObject = createSwagger(app);
+  SwaggerModule.setup('api/swagger', app, document);
+  app.enableCors();
+
+  return app;
+};
+
+// swaggerの作成
+export const createSwagger = (app: INestApplication) => {
+  const config = new DocumentBuilder().setTitle('portfolio_app 仕様').build();
+
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
+  return document;
+};
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,30 +1,11 @@
-import { INestApplication, ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import { writeFile } from 'fs/promises';
-import { AppModule } from './app.module';
-// import { isDev } from './lib/checkEnv';
+import { createApp, createSwagger } from './createApp';
 
 async function bootstrap() {
-  const app: INestApplication = await NestFactory.create(AppModule);
+  const app = await createApp();
+  const document = createSwagger(app);
 
-  // http://localhost:3031/apiのようにdefault
-  app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-
-  // if (isDev) {
-  const config = new DocumentBuilder()
-    .setTitle('portfolio_app ドキュメント')
-    .build();
-
-  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/swagger', app, document);
-  // }
-  app.enableCors();
-
-  // if (isDev) {
   await writeFile('./swagger.json', JSON.stringify(document, null, '  '));
-  // }
 
   await app.listen(3031);
 }
